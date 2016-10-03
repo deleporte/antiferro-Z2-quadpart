0e4325a0b2036eb568b2d1e3bbf2baca6e4390ea
deleporte <alix.deleporte@gmx.fr>
2016-10-03 09:11:12 +0200

    Store vectors & compute correlations

--- spinhalf.py
+++ spinhalf.py
@@ -1,5 +1,6 @@
 import scipy.sparse
 import numpy as np
+import scipy.sparse.linalg
 
 def save_sparse_csr(filename,array):
     np.savez(filename, data=array.data, indices=array.indices,
@@ -18,6 +19,45 @@ def load_sparse_coo(filename):
     loader=np.load(filename)
     return coo_matrix((loader['data'],(loader['row'],loader['col'])),
                        loader['shape'])
+
+def mean(j,vector):
+    #returns <Sxj>,<Szj>
+    mn=[0,0]
+    for i in range(2**16):
+        state=digits(2,i)
+        while len(state)<16:
+            state.append(0)
+        varstate=state[:]
+        varstate[j]=1-varstate[j]
+        mn[0]+=vector[i]*vector[number(2,varstate)]
+        #mean[1]-=vector[i]*vector[number(2,varstate)] #?
+        mn[1]+=vector[i]*vector[i]*(2*state[j]-1)
+    return mn
+
+def correlations(j1,j2,vector):
+    #by definition it is <SiSj>-<Si><Sj>
+    #it is not a value, but a 2x2 matrix
+    correl=np.array([[-mean(j1,vector)[0]*mean(j2,vector)[0],
+                      -mean(j1,vector)[0]*mean(j2,vector)[1]],
+                     [-mean(j1,vector)[1]*mean(j2,vector)[0],
+                      -mean(j1,vector)[1]*mean(j2,vector)[1]]])
+    for i in range(2**16):
+        state=digits(2,i)
+        while len(state)<16:
+            state.append(0)
+        varstate1=state[:]
+        varstate1[j1]=1-varstate1[j1]
+        varstate2=state[:]
+        varstate2[j2]=1-varstate2[j2]
+        varstate3=state[:]
+        varstate3[j1]=1-varstate3[j1]
+        varstate3[j2]=1-varstate3[j2]
+        correl[0,0]+=vector[i]*vector[number(2,varstate3)]
+        correl[0,1]+=vector[i]*vector[number(2,varstate1)]*(2*state[j2]-1)
+        correl[1,0]+=vector[i]*vector[number(2,varstate2)]*(2*state[j1]-1)
+        correl[1,1]+=vector[i]*vector[i]*(2*state[j1]-1)*(2*state[j2]-1)
+    return correl
+        
 
 def density(j1,j2,vector):
     d=0.
@@ -99,9 +139,20 @@ def fullmatrix(delta=1.):
 
 def transformmatrix(M,delta):
     for i in range(65536):
-        M[i,i]=-1.*delta*M[i,i]
+        M[i,i]=-1.*float(delta)*M[i,i]
     
+def storevectors(delta,Nb_values=100):
+    M=load_sparse_csr('matrix-1.0.npz')
+    transformmatrix(M,delta)
+    val,vect=scipy.sparse.linalg.eigsh(M,Nb_values)
+    filename='vectors'+str(delta)+'.npz'
+    np.savez(filename,values=val,vectors=vect)
 
+def loadvectors(delta):
+    filename='vectors'+str(delta)+'.npz'
+    loader=np.load(filename)
+    return loader['values'],loader['vectors']
+    
 def storedata():
     deltas=0.1*np.array(range(21))-1.
     for delta in deltas:
